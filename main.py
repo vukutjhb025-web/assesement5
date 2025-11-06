b207d0fe9b1b756a2eebd3d31ca0fe48c23ce7e5
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2025-11-06 16:36:21 +0200

    Assessment5

--- main.py
+++ main.py
@@ -10,7 +10,26 @@ def sum_numbers(nums: list[int]) -> int:
     Example:
         sum_numbers([2, -3, 5, -1]) → (2 + 5) - (3 + 1) = 3
     """
-    pass
+    # i = 0 
+    # while i > 0:
+
+    sum = 0
+    subtract = 0
+    
+    for i in nums:
+        if i >= 0:          
+            sum = sum + i     
+        elif i < 0:
+            i = i * -1
+            subtract = subtract + i
+    return sum - subtract
+
+# sum = 0
+# for i in range(1, 4):
+#     sum += i
+# print(sum)
+
+print(sum_numbers([2,-3,5,-1]))
 
 def is_anagram(str1: str, str2: str) -> bool:
     """
@@ -23,7 +42,12 @@ def is_anagram(str1: str, str2: str) -> bool:
         Input → str1 = "hello", str2 = "world"
         Output → False
     """
-    pass
+    for i in str1.lower():
+        if i in str2.lower():
+            return True
+        else:
+            return False
+
 
 def fibonacci_series(n_terms: int) -> List[int]:
     """
@@ -45,7 +69,28 @@ def prime_factors(n: int) -> List[int]:
         Input → 84
         Output → [2, 2, 3, 7]
     """
-    pass
+    factors = []
+    not_primes = []
+    primes = []
+    for i in range(1,n + 1):
+        if n % i == 0:
+            factors.append(i)
+    for i in factors:
+        for j in range(2, i):
+            print(j)
+            if i % j == 0:
+                not_primes.append(j)
+            else:
+                primes.append(j)
+    return primes
+    # factors = []
+    # prime = 2,3,5,7,11,13,17
+    # for i in prime:
+    #     if i % n == 0:           
+    #     if int(n) // int(prime):
+    #         return factors.append(i)
+
+print(prime_factors(84))
 
 def create_pyramid(n):
     """
@@ -73,7 +118,20 @@ def create_multiplication_square(n: int) -> List[str]:
         Input → 3
         Output → ['1 2 3', '2 4 6', '3 6 9']
     """
-    pass
+    user = int(input("Input a number :"))
+    string = []
+
+
+    for i in range(1,11):
+        for n in range(i,user + 1):
+            print(f"{i} {n} {i*n}")
+
+        result = f"{i}{n}{i*n}"
+        result = list(result)
+        return result
+               
+
+# print(create_multiplication_square(3))
 
 
 def create_diamond(n):
